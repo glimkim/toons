93a89c9be61cd67bcfccc2419ac1379b9a9efdcd
glimkim <limkimdev@gmail.com>
2022-05-10 14:03:55 +0900

    feat: initialise toons project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ToonsThemeProvider } from 'toons-components';
+
+function App() {
+  return (
+    <ToonsThemeProvider themeType='DEFAULT'>
+      <div></div>
+    </ToonsThemeProvider>
+  );
+}
+
+export default App;
