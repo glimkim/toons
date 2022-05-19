fa8072d42f741d9d41fcd480770cb8bc704973f8
glimkim <limkimdev@gmail.com>
2022-05-19 12:40:50 +0900

    feat: apply webfonts

---
+++ src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle, css } from 'styled-components';
+import { ToonsGlobalCss } from 'toons-components';
+
+const fonts = css`
+  @font-face {
+    font-family: 'Black Han Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-display: fallback;
+    src: local('Black Han Sans'),
+      url(../assets/fonts/BlackHanSans-Regular.woff2) format('woff2'),
+      url(../assets/fonts/BlackHanSans-Regular.woff) format('woff');
+  }
+  @font-face {
+    font-family: 'Noto Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-display: fallback;
+    src: local('Noto Sans'),
+      url(../assets/fonts/NotoSansKR-Regular.woff2) format('woff2'),
+      url(../assets/fonts/NotoSansKR-Regular.woff) format('woff');
+  }
+  @font-face {
+    font-family: 'Noto Sans';
+    font-style: bold;
+    font-weight: 700;
+    font-display: fallback;
+    src: local('Noto Sans'),
+      url(../assets/fonts/NotoSansKR-Bold.woff2) format('woff2'),
+      url(../assets/fonts/NotoSansKR-Bold.woff) format('woff');
+  }
+`;
+
+const GloablStyles = createGlobalStyle`
+    ${ToonsGlobalCss};
+    ${fonts};
+    *{
+        font-family: 'Noto Sans', sans-serif;
+        font-weight: 400;
+    }
+    *.wrapper{
+        width: 1280px;
+        max-width: 100%;
+    }
+
+    @media screen and (max-width: 1339px){
+        *.wrapper{
+            padding: 0 1rem;
+        }
+    }
+`;
+
+export default GloablStyles;
