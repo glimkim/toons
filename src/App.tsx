c2c548204c66afd57f4d3ce7d841818b7fa73bf1
glimkim <limkimdev@gmail.com>
2022-06-10 13:44:06 +0900

    feat: apply react-query library

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import { theme } from 'toons-components';
 import GlobalStyles from '@styles/global';
 import AppRouter from '@routes/AppRouter';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {},
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <AppRouter />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <AppRouter />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
