418411bd1e82dda7fdd398f04dff30c42e7e31c1
glimkim <limkimdev@gmail.com>
2022-07-18 17:30:23 +0900

    feat: create getWebtoonsAPI

---
+++ src/apis/webtoons.ts
@@ -0,0 +1,20 @@
+import Axios from './config';
+import {
+  Contents,
+  DayOfWeek,
+  Platform,
+  WebtoonItemResponseDTO,
+} from './DTO/webtoons';
+
+export const getWebtoonsAPI = async (
+  platfrom: Platform,
+  dayOfWeek?: DayOfWeek,
+) => {
+  return (
+    await Axios.get<Contents<WebtoonItemResponseDTO>>(
+      `/api/webtoons?platform=${platfrom}${
+        dayOfWeek && `&day-of-week=${dayOfWeek}`
+      }`,
+    )
+  ).data;
+};
