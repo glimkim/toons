180d14dd8112ed639d7509876fecd3d2ff77ee85
glimkim <limkimdev@gmail.com>
2022-07-25 15:10:29 +0900

    feat: create api custom hook to add toNotify prop depending on alarm List (redux store)

---
+++ src/hooks/api/useWebtoonList.ts
@@ -0,0 +1,37 @@
+import { Contents, WebtoonItemResponseDTO } from '@apis/DTO/webtoons';
+import { getWebtoonsAPI } from '@apis/webtoons';
+import { useQuery, useMutation } from 'react-query';
+import { StoreState } from '@store/root';
+import { useSelector } from 'react-redux';
+
+interface WebtoonItem extends WebtoonItemResponseDTO {
+  toNotify: boolean;
+}
+
+function useWebtoonList() {
+  const { alarms } = useSelector((state: StoreState) => state);
+
+  const naverWebtoonsQuery = useQuery<
+    Contents<WebtoonItemResponseDTO>,
+    unknown,
+    WebtoonItem[]
+  >(['naver-webtoon-list', alarms], () => getWebtoonsAPI('NAVER'), {
+    select: (res) => {
+      return res.content.map((item) =>
+        alarms.some((_item) => _item.webtoonName === item.name)
+          ? {
+              ...item,
+              toNotify: true,
+            }
+          : {
+              ...item,
+              toNotify: false,
+            },
+      );
+    },
+  });
+
+  return { naverWebtoonsQuery };
+}
+
+export default useWebtoonList;
