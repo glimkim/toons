74cae799d4834da062a181367ce053ef1b15dca2
glimkim <limkimdev@gmail.com>
2022-07-26 15:50:03 +0900

    feat: create alarm.ts and DTO file for alarm related API

---
+++ src/apis/DTO/alarms.ts
@@ -0,0 +1,7 @@
+export interface AlarmItem {
+  webtoonName: string;
+}
+
+export interface AddAlarmItemRequestDTO {
+  webtoonId: number;
+}
---
+++ src/apis/alarms.ts
@@ -0,0 +1,35 @@
+import Axios from './config';
+import { AlarmItem, AddAlarmItemRequestDTO } from './DTO/alarms';
+
+const getHeaders = (token: string) => {
+  return {
+    Authorization: `Bearer ${token}`,
+  };
+};
+
+export const getAlarmListAPI = async (token: string) => {
+  return (
+    await Axios.get<AlarmItem[]>('/api/alarms', {
+      headers: getHeaders(token),
+    })
+  ).data;
+};
+
+export const addAlarmItemAPI = async (
+  token: string,
+  alarmItem: AddAlarmItemRequestDTO,
+) => {
+  return (
+    await Axios.put('/api/alarms', alarmItem, {
+      headers: getHeaders(token),
+    })
+  ).data;
+};
+
+export const deleteAlarmItemAPI = async (token: string, webtoonId: number) => {
+  return (
+    await Axios.delete(`/api/alarms/${webtoonId}`, {
+      headers: getHeaders(token),
+    })
+  ).data;
+};
