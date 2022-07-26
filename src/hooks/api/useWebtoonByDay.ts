724a006f0b2a3aed7cd82d63164256e1ec7a08d5
glimkim <limkimdev@gmail.com>
2022-07-26 16:04:43 +0900

    feat: create use WebtoonByDay hook

---
+++ src/hooks/api/useWebtoonByDay.ts
@@ -0,0 +1,34 @@
+import { Platform } from '@apis/DTO/webtoons';
+import { getWebtoonsAPI } from '@apis/webtoons';
+import { DayOfWeek } from './../../apis/DTO/webtoons';
+import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
+import { StoreState } from '@store/root';
+
+function useWebtoonsByDay(platform: Platform, dayOfWeek: DayOfWeek) {
+  const { alarms: alarmList } = useSelector((state: StoreState) => state);
+
+  const webtoonsByDayQuery = useQuery(
+    [`${dayOfWeek}-list`, alarmList],
+    () => getWebtoonsAPI(platform, dayOfWeek),
+    {
+      select: (res) => {
+        return res.content.map((item) =>
+          alarmList.some((_item) => _item.webtoonName === item.name)
+            ? {
+                ...item,
+                toNotify: true,
+              }
+            : {
+                ...item,
+                toNotify: false,
+              },
+        );
+      },
+    },
+  );
+
+  return { webtoonsByDayQuery };
+}
+
+export default useWebtoonsByDay;
