1f3ce296b5093897a54e36f456e8dabf1762ccf3
glimkim <limkimdev@gmail.com>
2022-08-09 17:19:48 +0900

    feat: improve mypage view using tab bar from toons-components

--- src/pages/myPage.tsx
+++ src/pages/myPage.tsx
@@ -3,20 +3,41 @@ import { paddingUnderHeader } from '@styles/css';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
+import { TabBar } from 'toons-components';
 
 function MyPage() {
+  const tabs = [
+    {
+      title: 'ALL',
+      contents: <div>contents here</div>,
+    },
+    {
+      title: 'NAVER',
+      contents: <div>contents here</div>,
+    },
+    {
+      title: 'KAKAO',
+      contents: <div>contents here</div>,
+    },
+  ];
   return (
     <PageLayout>
       <Helmet title="Toons | My Page" />
-      {/* <title>Toons | My Page</title>
-      </Helmet> */}
-      <MyPageContainer>my page</MyPageContainer>
+      <MyPageContainer>
+        <div className="wrapper">
+          <TabBar headTitle="Your Notification" tabs={tabs} />
+        </div>
+      </MyPageContainer>
     </PageLayout>
   );
 }
 
 const MyPageContainer = styled.div`
-  ${paddingUnderHeader}
+  ${paddingUnderHeader};
+  div.wrapper {
+    padding: 3rem 0;
+  }
+  background-color: ${({ theme: { colors } }) => colors.main};
 `;
 
 export default MyPage;
