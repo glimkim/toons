95273ed8bfbb3108b6c8ea1c106e980845d91b60
glimkim <limkimdev@gmail.com>
2022-08-10 14:43:43 +0900

    feat: develop my page view
    Added profile box view

--- src/pages/myPage.tsx
+++ src/pages/myPage.tsx
@@ -1,11 +1,15 @@
 import PageLayout from '@layout/pageLayout';
+import { StoreState } from '@store/root';
 import { paddingUnderHeader } from '@styles/css';
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { TabBar } from 'toons-components';
+import { Icon, TabBar } from 'toons-components';
 
 function MyPage() {
+  const user = useSelector((state: StoreState) => state.user);
+
   const tabs = [
     {
       title: 'ALL',
@@ -25,6 +29,21 @@ function MyPage() {
       <Helmet title="Toons | My Page" />
       <MyPageContainer>
         <div className="wrapper">
+          <ProfileContainer>
+            <h6>
+              Hello,
+              <br />
+              Lim!{' '}
+              <button type="button">
+                <Icon icon="Edit" />
+              </button>
+            </h6>
+            <ul className="userInfo">
+              <li>ltbllim@gmail.com</li>
+              <li>|</li>
+              <li>010-2309-9296</li>
+            </ul>
+          </ProfileContainer>
           <TabBar headTitle="Your Notification" tabs={tabs} />
         </div>
       </MyPageContainer>
@@ -32,10 +51,28 @@ function MyPage() {
   );
 }
 
+const ProfileContainer = styled.div`
+  margin-bottom: 3rem;
+  h6 {
+    font-size: 3rem;
+    font-family: 'Black Han Sans';
+  }
+  ul.userInfo {
+    display: flex;
+    gap: 0.36rem;
+    li {
+      font-size: 1.125rem;
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.gray40};
+    }
+  }
+`;
+
 const MyPageContainer = styled.div`
   ${paddingUnderHeader};
   div.wrapper {
-    padding: 3rem 0;
+    padding-top: 3rem;
+    padding-bottom: 3rem;
   }
   background-color: ${({ theme: { colors } }) => colors.main};
 `;
