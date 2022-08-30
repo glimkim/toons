4fc60eba538dc98d41397b8b6b60ff1a1e7c2055
glimkim <limkimdev@gmail.com>
2022-08-30 14:52:51 +0900

    refactor: remove wrapping alarmList in this hook to avoid calling api unnecessarily

--- src/hooks/api/useWebtoonByDay.ts
+++ src/hooks/api/useWebtoonByDay.ts
@@ -2,35 +2,16 @@ import { Platform } from '@apis/DTO/webtoons';
 import { getWebtoonsAPI } from '@apis/webtoons';
 import { DayOfWeek } from './../../apis/DTO/webtoons';
 import { useQuery } from 'react-query';
-import { useSelector } from 'react-redux';
-import { StoreState } from '@store/root';
 import { useState, useMemo } from 'react';
 
 function useWebtoonsByDay(platform: Platform) {
-  const { alarms: alarmList } = useSelector((state: StoreState) => state);
   const [dayOfWeek, setDayOfWeek] = useState<DayOfWeek>('MONDAY');
   const selectedPlatform = useMemo(() => {
     return platform;
   }, [platform]);
 
-  const webtoonsByDayQuery = useQuery(
-    [`${dayOfWeek}-list`, alarmList],
-    () => getWebtoonsAPI(selectedPlatform, dayOfWeek),
-    {
-      select: (res) => {
-        return res.content.map((item) =>
-          alarmList.some(({ webtoonDTO: _item }) => _item.name === item.name)
-            ? {
-                ...item,
-                toNotify: true,
-              }
-            : {
-                ...item,
-                toNotify: false,
-              },
-        );
-      },
-    },
+  const webtoonsByDayQuery = useQuery([`${dayOfWeek}-list`], () =>
+    getWebtoonsAPI(selectedPlatform, dayOfWeek),
   );
 
   return { webtoonsByDayQuery, setDayOfWeek, dayOfWeek };
